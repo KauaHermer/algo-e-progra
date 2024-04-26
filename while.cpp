ebe1775eac6391f2021aa7f9b9f4592c78649fb8
Kauã Hermer <127157339+KauaHermer@users.noreply.github.com>
2024-04-25 21:26:59 -0300

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,31 @@
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    float soma = 0;
+    float nota;
+    float media;
+    int contador = 0;
+    int n;
+    
+    cout << "Dígite a quatidade de notas desejadas: ";
+    cin >> n;
+    
+    while(contador < n){
+        
+        cout << "\nDígite a nota " << (contador + 1) << ": ";
+        cin >> nota;
+        soma = soma + nota;
+        contador++;
+    }
+    
+    media = soma / n;
+    cout << "\nA sua média é: " << media << endl;
+
+    return 0;
+}
+
+    return 0;
+}
